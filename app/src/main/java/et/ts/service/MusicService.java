efe914aa427e7a2ab604f8514c49b4eb7961824b
nly <test@nly.com>
2019-01-30 10:17:44 +0800

    基本功能实现

---
+++ app/src/main/java/et/ts/service/MusicService.java
@@ -0,0 +1,108 @@
+package et.ts.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+
+
+public class MusicService extends Service {
+
+    String [] mMusics=new String[]{
+            "/sdcard/wangyiMusic/cd.mp3",
+            "/sdcard/wangyiMusic/ce.mp3",
+            "/sdcard/wangyiMusic/cf.mp3",
+            "/sdcard/wangyiMusic/cg.mp3",
+            "/sdcard/wangyiMusic/ch.mp3"
+    };
+
+
+    private  int currentMusicId=0;
+    private  int myMusicSize=mMusics.length;
+
+    public MediaPlayer mediaPlayer;
+    public boolean tag = false;
+
+    public MusicService() {
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(mMusics[currentMusicId]);
+            mediaPlayer.prepare();
+            mediaPlayer.setLooping(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //  通过 Binder 来保持 Activity 和 Service 的通信
+    public MyBinder binder = new MyBinder();
+    public class MyBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    public void playOrPause() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+        }
+    }
+
+    public void pre(){
+        currentMusicId=((currentMusicId-1)+myMusicSize)%myMusicSize;
+        stop();
+        Log.d("niuliangyi","hhhhhhhhhh"+currentMusicId);
+        try {
+            mediaPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    public void next(){
+        currentMusicId=((currentMusicId+1)+myMusicSize)%myMusicSize;
+        stop();
+        Log.d("niuliangyi","hhhhhhhhhh"+currentMusicId);
+        try {
+            mediaPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(mMusics[currentMusicId]);
+                mediaPlayer.prepare();
+                mediaPlayer.seekTo(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+}
+
